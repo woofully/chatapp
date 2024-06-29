aad70c934c376212d2813a970856264758ace551
Louis <louis@bonduet.com>
2024-06-29 13:58:38 +0800

    first commit

---
+++ app/action.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import { getServerSession } from "next-auth";
+import { prisma } from "./lib/db";
+import { authOptions } from "./lib/auth";
+
+export async function postData(formdata: FormData) {
+  "use server";
+
+  const Pusher = require("pusher");
+
+  const session = await getServerSession(authOptions);
+  const message = formdata.get("message");
+
+  const data = await prisma.message.create({
+    data: {
+      message: message as string,
+      email: session?.user?.email,
+    },
+    include: {
+      User: {
+        select: {
+          name: true,
+          image: true,
+        },
+      },
+    },
+  });
+
+  const pusher = new Pusher({
+    appId: process.env.PUSHER_APP_ID,
+    key: process.env.NEXT_PUBLIC_PUSHER_KEY,
+    secret: process.env.PUSHER_SECRET,
+    cluster: "ap1",
+    useTLS: true,
+  });
+
+  pusher.trigger("chat", "hello", {
+    message: `${JSON.stringify(data)}\n\n`,
+  });
+}
